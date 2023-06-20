60b572ed71118865748558c6a66d9425ac88a60d
Aditya Singh <asaditya2002@gmail.com>
2023-06-20 22:24:22 +0530

    Completed Airdrop functionality

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -3,18 +3,35 @@ import React from "react";
 import { NextPage } from "next";
 import { Navbar, DetailsForm } from "../../Components";
 import { getBalance } from "./getBalance";
+import { airdrop } from "./Airdrop";
 
 // /Accounts page
 const page: NextPage = () => {
+
   const [balance, setBalance] = React.useState<number>();
-  
+  const [pubKey, setPubKey] = React.useState<string>(""); 
+
   const handleSubmit = async(userInputKey:string) => {
+    setPubKey(userInputKey);
     const res = await getBalance(userInputKey);
+    if(res == -1) {
+      alert("Please enter a valid public key");
+      return;
+    }
     setBalance(res);
   };
 
   const handleAirDrop = async() => {
-    console.log("airdrop")
+    if(pubKey.length != 44) {
+      alert("Please enter a valid public key");
+      return;
+    }
+    return await airdrop(pubKey).then((res) => {
+      alert("Airdrop Successful");
+      handleSubmit(pubKey);
+    }).catch((err) => {
+      alert("Airdrop Failed");
+    });
   }
 
   return (
